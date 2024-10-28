8f1e3bffd3e0ccd4d235e82d42c549c2f3f17ec8
Senay Gebru <neezus7@gmail.com>
2024-10-27 22:10:42 -0500

    rm backend

--- health-shield-website/backend/models/Doctor.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const DoctorSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    location: { type: String, required: true },
-    specialty: { type: String, required: true }
-});
-
-module.exports = mongoose.model('Doctor', DoctorSchema);
